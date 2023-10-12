22b45403a1894f4d4a05768cae48faf93743052c
YouYou94 <ybh4115@gmail.com>
2023-10-12 20:16:22 +0900

    init: Layout

---
+++ src/components/index.tsx
@@ -0,0 +1,2 @@
+/* Layout */
+export { DefaultLayout, ColumnLayout } from './layout';
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const DefaultLayout = styled.div`
+  flex: 1;
+
+  display: flex;
+`;
+
+export const ColumnLayout = styled(DefaultLayout)`
+  flex-direction: column;
+`;
