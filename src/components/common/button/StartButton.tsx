63c48b6f34266c8d7b8adfd8d14cee323d49a7d5
YouYou94 <ybh4115@gmail.com>
2023-10-16 20:10:46 +0900

    Feat: Start Button Mouse Event

--- src/components/common/button/StartButton.tsx
+++ src/components/common/button/StartButton.tsx
@@ -1,11 +1,26 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
 import Start_Default from '../../../assets/button/Start_Default.png';
+import Start_Hover from '../../../assets/button/Start_Hover.png';
 
 export const StartButton = () => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+
+  const MouseOverHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setIsHover(true);
+  };
+
+  const MouseOutHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setIsHover(false);
+  };
+
   return (
     <Layout>
-      <Button>
-        <ButtonImg src={Start_Default} alt="Start Button Image" />
+      <Button onMouseOver={MouseOverHandler} onMouseOut={MouseOutHandler}>
+        <ButtonImg
+          src={isHover ? Start_Hover : Start_Default}
+          alt="Start Button Image"
+        />
       </Button>
     </Layout>
   );
