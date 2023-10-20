9d9aa93f02b673bc5d5a4249c74710510421d5f8
YouYou94 <ybh4115@gmail.com>
2023-10-20 18:49:02 +0900

    Add: Home Route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Start } from '../pages';
+import { Home, Start } from '../pages';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/my-pokemon-tamagotchi" element={<Start />} />
+        <Route path="/my-pokemon-tamagotchi/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
