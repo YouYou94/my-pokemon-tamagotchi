ad5c6f5592b29c45d370bcd082cc281dac077df9
YouYou94 <ybh4115@gmail.com>
2023-10-27 22:51:44 +0900

    Design: Display Component

--- src/components/display/Styled.tsx
+++ src/components/display/Styled.tsx
@@ -1,1 +1,8 @@
 import styled from '@emotion/styled';
+
+export const Layout = styled.section`
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+`;
--- src/components/display/index.tsx
+++ src/components/display/index.tsx
@@ -1,3 +1,5 @@
+import { Layout } from './Styled';
+
 export const Display = () => {
-  return <></>;
+  return <Layout></Layout>;
 };
